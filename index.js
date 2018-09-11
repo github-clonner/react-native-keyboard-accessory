9f8dd09c8521bd100ef37d723f87d0f5098a2eec
aladdin <aladdin@ddex.io>
2018-09-11 15:33:25 +0800

    Fixed index.js path bug

---
+++ index.js
@@ -0,0 +1,9 @@
+import KeyboardAccessoryView from './src/KeyboardAccessoryView';
+import KeyboardAccessoryNavigation from './src/KeyboardAccessoryNavigation';
+import KeyboardAwareTabBarComponent from './src/KeyboardAwareTabBarComponent';
+
+export {
+  KeyboardAccessoryView,
+  KeyboardAccessoryNavigation,
+  KeyboardAwareTabBarComponent
+}
